d5ed8cb8d779005582eb82013cdef2234f651994
SangameshS18 <sangameshsonnad070@gmail.com>
2025-11-20 22:57:49 +0530

    initial commit

---
+++ src/com/IfElseExample.java
@@ -0,0 +1,16 @@
+package com;
+
+public class IfElseExample {
+    public static void main(String[] args) {
+        int number = 10;
+
+        // If-Else Statement
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+
+        
+    }
+}
